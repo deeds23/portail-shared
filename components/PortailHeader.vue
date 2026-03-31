8d8e30fc228c2ce9903abfca0f8bca53e9c07732
BILLON Didier <d.billon@sudouest.fr>
2026-03-31 07:28:31 +0000

    first commit

---
+++ components/PortailHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-screen w-full flex flex-col bg-gray-50 overflow-hidden">
+    <nav class="bg-neutral-primary w-full z-50 border-b border-default shadow-sm shrink-0">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" class="flex items-center space-x-3">
+          <img src="~/assets/pictures/logo_SO.png" class="h-7" alt="Logo" />
+          <span class="self-center text-xl font-semibold whitespace-nowrap">Portail SO</span>
+        </a>
+
+        <div v-if="userStore.authenticated" class="w-full md:block md:w-auto">
+          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base md:flex-row md:space-x-8 md:mt-0 md:border-0">
+          
+            <li>
+              <a href="/app152/" class="text-blue-600 font-bold hover:underline">
+                Application Métier
+              </a>
+            </li>
+            <li><a href="/profile" class="text-blue-600 font-bold hover:underline">Profil</a></li>
+            <li><button @click="userStore.logout" class="px-3 py-1 bg-red-600 text-white rounded">Logout</button></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <main class="flex-1 w-full flex flex-col overflow-auto">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from '~/stores/user'
+const userStore = useUserStore()
+onMounted(async () => {
+  await userStore.fetchUser()
+})
+</script>
