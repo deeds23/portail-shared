e61e46cf05895aee14d903747bca5ed3912e3ffd
BILLON Didier <d.billon@sudouest.fr>
2026-04-01 08:45:44 +0000

    mise à jour bouton

--- components/PortailHeader.vue
+++ components/PortailHeader.vue
@@ -8,48 +8,37 @@
         </a>
 
         <div v-if="userStore.authenticated" class="w-full md:block md:w-auto">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base md:flex-row md:space-x-8 md:mt-0 md:border-0">
-          
-            <li>
-                 <header class="text-white p-4 shadow-md w-full shrink-0">
-      <div class="flex justify-between items-center px-4 max-w-screen-xl mx-auto">
-        <div class="flex gap-3">
-          <button 
-            @click="allerVersApp152" 
-            class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white"
-          >
-            🚀 App 152
-          </button>
-          <button 
-            @click="allerVersApp1522" 
-            class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white"
-          >
-            🚀 App 152
-          </button>
-          <button 
-            @click="allerVersApp1523" 
-            class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white"
-          >
-            🚀 App 152
-          </button>
-          
-          <button 
-            @click="activeApp = 'voxa'" 
-            :class="activeApp === 'voxa' ? 'bg-emerald-700 ring-2 ring-white' : 'bg-emerald-600 hover:bg-emerald-700'"
-            class="px-4 py-2 rounded transition text-white"
-          >
-            🐍 Voxa
-          </button>
+          <ul
+            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base md:flex-row md:space-x-8 md:mt-0 md:border-0">
 
-          <button 
-            @click="activeApp = null" 
-            class="bg-gray-600 hover:bg-gray-700 px-4 py-2 rounded transition ml-4 text-white"
-          >
-            Accueil
-          </button>
-        </div>
-      </div>
-    </header>
+            <li>
+              <div class="flex justify-between items-center px-4 max-w-screen-xl mx-auto">
+                <div class="flex gap-3">
+                  <nuxtlink to="/app152/carte"
+                    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
+                    🚀 App 152
+                  </nuxtlink>
+                  <nuxtlink to="/app152/simple"
+                    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
+                    🚀 App 152
+                  </nuxtlink>
+                  <nuxtlink to="/app152/formulaire"
+                    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
+                    🚀 App 152
+                  </nuxtlink>
+
+                  <button @click="activeApp = 'voxa'"
+                    :class="activeApp === 'voxa' ? 'bg-emerald-700 ring-2 ring-white' : 'bg-emerald-600 hover:bg-emerald-700'"
+                    class="px-4 py-2 rounded transition text-white">
+                    🐍 Voxa
+                  </button>
+
+                  <button @click="activeApp = null"
+                    class="bg-gray-600 hover:bg-gray-700 px-4 py-2 rounded transition ml-4 text-white">
+                    Accueil
+                  </button>
+                </div>
+              </div>
 
             </li>
             <li><a href="/profile" class="text-blue-600 font-bold hover:underline">Profil</a></li>
@@ -66,26 +55,26 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { useUserStore } from '~/stores/user'
-const userStore = useUserStore()
-onMounted(async () => {
-  await userStore.fetchUser()
-})
-// On gère uniquement l'état de l'Iframe (pour Voxa)
-const activeApp = ref(null)
+  import { onMounted } from 'vue'
+  import { useUserStore } from '~/stores/user'
+  const userStore = useUserStore()
+  onMounted(async () => {
+    await userStore.fetchUser()
+  })
+  // On gère uniquement l'état de l'Iframe (pour Voxa)
+  const activeApp = ref(null)
 
-// Fonction magique de Nuxt pour l'App 152
-const allerVersApp152 = () => {
-  // naviagteTo change l'URL du navigateur instantanément vers votre nouvelle page !
-  navigateTo('/app152/carte')
-}
-const allerVersApp1522 = () => {
-  // naviagteTo change l'URL du navigateur instantanément vers votre nouvelle page !
-  navigateTo('/app152/simple')
-}
-const allerVersApp1523 = () => {
-  // naviagteTo change l'URL du navigateur instantanément vers votre nouvelle page !
-  navigateTo('/app152/formulaire')
-}
+  // Fonction magique de Nuxt pour l'App 152
+  const allerVersApp152 = () => {
+    // naviagteTo change l'URL du navigateur instantanément vers votre nouvelle page !
+    navigateTo('/app152/carte')
+  }
+  const allerVersApp1522 = () => {
+    // naviagteTo change l'URL du navigateur instantanément vers votre nouvelle page !
+    navigateTo('/app152/simple')
+  }
+  const allerVersApp1523 = () => {
+    // naviagteTo change l'URL du navigateur instantanément vers votre nouvelle page !
+    navigateTo('/app152/formulaire')
+  }
 </script>
