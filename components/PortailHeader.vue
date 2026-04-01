bae2b5596e4ad1d462d495034717b07655e5202c
BILLON Didier <d.billon@sudouest.fr>
2026-04-01 12:04:21 +0000

    mise à jour portailheader

--- components/PortailHeader.vue
+++ components/PortailHeader.vue
@@ -2,7 +2,7 @@
   <div class="h-screen w-full flex flex-col bg-gray-50 overflow-hidden">
     <nav class="bg-neutral-primary w-full z-50 border-b border-default shadow-sm shrink-0">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <NuxtLink to="/" class="flex items-center space-x-3">
+        <NuxtLink to="/" external class="flex items-center space-x-3">
           <img src="/logo_SO.png" class="h-7" alt="Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap">Portail SO APP</span>
         </NuxtLink>
@@ -12,7 +12,7 @@
             <li>
               <div class="flex justify-between items-center px-4 max-w-screen-xl mx-auto">
                 <div class="flex gap-3">
-                  <NuxtLink to="/app/carte" class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Carte</NuxtLink>
+                  <NuxtLink to="/app/carte" external class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Carte</NuxtLink>
                   <NuxtLink to="/app/formulaire" class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Formulaire</NuxtLink>
 
                   <NuxtLink to="/app152/simple" external class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Simple (152)</NuxtLink>
@@ -22,7 +22,7 @@
                 </div>
               </div>
             </li>
-            <li><NuxtLink to="/profile" class="text-blue-600 font-bold hover:underline">Profil</NuxtLink></li>
+            <li><NuxtLink to="/profile" external class="text-blue-600 font-bold hover:underline">Profil</NuxtLink></li>
             <li><button @click="userStore.logout" class="px-3 py-1 bg-red-600 text-white rounded">Logout</button></li>
           </ul>
         </div>
@@ -36,16 +36,10 @@
 </template>
 
 <script setup lang="ts">
-  import { onMounted, ref } from 'vue'
+  import { ref } from 'vue'
   import { useUserStore } from '~/stores/user'
   
   const userStore = useUserStore()
-  const activeApp = ref(null)
+  const activeApp = ref<string | null>(null)
 
-  // On remet la vérification ici pour débloquer le v-if !
-  onMounted(async () => {
-    if (!userStore.authenticated) {
-      await userStore.fetchUser()
-    }
-  })
 </script>
