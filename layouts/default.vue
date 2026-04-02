af2f406bd1b394f8ce6d6f51c8b3634868454578
BILLON Didier <d.billon@sudouest.fr>
2026-04-02 09:40:51 +0000

    maj default

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,20 +12,9 @@
             <li>
               <div class="flex justify-between items-center px-4 max-w-screen-xl mx-auto">
                 <div class="flex gap-3">
-<NuxtLink 
-    v-if="userStore.hasGroup('Test-acces-2')" 
-    to="/app/carte" 
-    external 
-    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
-    🚀 Carte
-  </NuxtLink>
-  <NuxtLink 
-    v-if="userStore.hasGroup('Test-acces-1')" 
-    to="/app/formulaire" 
-    external 
-    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
-    🚀 Formulaire
-  </NuxtLink>
+                  <NuxtLink to="/app/carte" external class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Carte</NuxtLink>
+                  <NuxtLink to="/app/formulaire" external class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Formulaire</NuxtLink>
+
                   <NuxtLink to="/app152/simple" external class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Simple (152)</NuxtLink>
                   <NuxtLink to="/profile" external class="bg-green-600 hover:bg-green-700 px-4 py-2 rounded transition text-white">Profil</NuxtLink>
                   <button @click="userStore.logout" class="bg-red-600 hover:bg-red-700 px-4 py-2 rounded transition text-white">Logout</button>
