68c3f92f127970055d8efa81686303df00e19e1d
BILLON Didier <d.billon@sudouest.fr>
2026-04-02 09:15:05 +0000

    maj middleware

--- middleware/auth.client.global.ts
+++ middleware/auth.client.global.ts
@@ -1,27 +1,29 @@
 import { useUserStore } from '~/stores/user'
 
 export default defineNuxtRouteMiddleware(async (to) => {
+  // 🚨 L'EXCEPTION VITALE : On laisse passer la page de login !
+  if (to.path === '/login') {
+    return
+  }
+
   const userStore = useUserStore()
   
-  // 1. On tente de récupérer l'utilisateur si le store est vide
   if (!userStore.authenticated) {
     await userStore.fetchUser()
   }
   
-  // 2. Si ça échoue (pas de cookie), on jette vers le login
   if (!userStore.authenticated) {
     console.warn("🔒 Middleware Layer : Non authentifié, redirection vers login.")
     return navigateTo('/login', { external: true })
   }
 
-  // 3. 🚨 LA MAGIE DU LAYER : Vérification dynamique du groupe
-  // On lit la propriété personnalisée "requiredGroup" que l'on définira sur nos pages
+  // Vérification dynamique du groupe
   const requiredGroup = to.meta.requiredGroup as string | undefined
 
   if (requiredGroup) {
     if (!userStore.hasGroup(requiredGroup)) {
       console.warn(`🔒 Middleware Layer : Accès refusé, le groupe "${requiredGroup}" est requis.`)
-      return navigateTo('/', { external: true }) // Retour à l'accueil
+      return navigateTo('/', { external: true })
     }
   }
 })
