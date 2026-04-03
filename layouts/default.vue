83f2774f548528d37831b9e4f87cc8515b2345fa
BILLON Didier <d.billon@sudouest.fr>
2026-04-03 05:21:00 +0000

    maj layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,7 +44,7 @@
 
 <script setup lang="ts">
   import { ref, onMounted } from 'vue'
-  import { useUserStore } from '~/stores/user'
+  //import { useUserStore } from '~/stores/user'
 
   const userStore = useUserStore()
 
