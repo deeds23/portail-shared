1d5482ab9dc2c0759331468c6846cee9222c1981
BILLON Didier <d.billon@sudouest.fr>
2026-04-03 06:08:02 +0000

    maj store

--- stores/user.ts
+++ stores/user.ts
@@ -34,7 +34,7 @@ export const useUserStore = defineStore('user', {
         console.error('Fetch user failed:', err)
       }
     }, */
-    async fetchUser() {
+    /* async fetchUser() {
       try {
         const res = await $fetch<any>('/api/auth/user', { 
           baseURL: '/', 
@@ -56,6 +56,23 @@ export const useUserStore = defineStore('user', {
       } catch (err) {
         this.setUser(null)
         console.error('Fetch user failed:', err)
+      }
+    }, */
+    async fetchUser() {
+      try {
+        // 🚨 AJOUT CRUCIAL : On force l'URL absolue vers le serveur 151
+        // Ainsi, même le 152 saura où aller chercher l'identité de l'utilisateur !
+        const res = await $fetch<any>('http://172.16.1.151:3000/api/auth/user', { 
+          credentials: 'include' 
+        })
+        
+        if (res && res.user) {
+          this.setUser(res.user)
+        } else {
+          this.setUser(null)
+        }
+      } catch (err) {
+        this.setUser(null)
       }
     },
     async logout() {
