a53e0438a8a5f72bfdfb1ead04f5b1ee13bec579
BILLON Didier <d.billon@sudouest.fr>
2026-04-18 19:17:25 +0000

    modififcation layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="h-screen w-full flex flex-col bg-gray-50 overflow-hidden">
     <nav class="bg-neutral-primary w-full z-50 border-b border-default shadow-sm shrink-0">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <NuxtLink to="/" external class="flex flew-auto items-center space-x-3">
+        <NuxtLink to="/" class="flex items-center space-x-3">
           <img src="/logo_SO.png" class="h-7" alt="Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap">Portail SO APP</span>
         </NuxtLink>
@@ -12,20 +12,20 @@
             <li>
               <div class="flex justify-between items-center px-4 max-w-screen-xl mx-auto">
                 <div class="flex gap-3">
-<NuxtLink 
-    v-if="userStore.hasGroup('Test-acces-2')" 
-    to="/app/carte" 
-    external 
-    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
-    🚀 Carte
-  </NuxtLink>
-  <NuxtLink 
-    v-if="userStore.hasGroup('Test-acces-1')" 
-    to="/app/formulaire" 
-    external 
-    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
-    🚀 Formulaire
-  </NuxtLink>
+                  <NuxtLink
+                    v-if="userStore.hasGroup('Test-acces-2')"
+                    to="/app/carte"
+                    external
+                    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
+                    Carte
+                  </NuxtLink>
+                  <NuxtLink
+                    v-if="userStore.hasGroup('Test-acces-1')"
+                    to="/kitchen/"
+                    external
+                    class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">
+                    Kitchen
+                  </NuxtLink>
                   <NuxtLink to="/app152/simple" external class="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded transition text-white">🚀 Simple (152)</NuxtLink>
                   <NuxtLink to="/profile" external class="bg-green-600 hover:bg-green-700 px-4 py-2 rounded transition text-white">Profil</NuxtLink>
                   <button @click="userStore.logout" class="bg-red-600 hover:bg-red-700 px-4 py-2 rounded transition text-white">Logout</button>
@@ -44,15 +44,11 @@
 </template>
 
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { useUserStore } from '~/stores/user'
-  
-  const userStore = useUserStore()
+const userStore = useUserStore()
 
-  // On force la vérification de l'utilisateur au chargement du Layout !
-  onMounted(async () => {
-    if (!userStore.authenticated) {
-      await userStore.fetchUser()
-    }
-  })
+onMounted(async () => {
+  if (!userStore.authenticated) {
+    await userStore.fetchUser()
+  }
+})
 </script>
